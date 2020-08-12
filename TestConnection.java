607b158064f4a159f3df26bb18db91cb40fab6af
mambora18 <69573856+mambora18@users.noreply.github.com>
2020-08-12 18:24:14 +0530

    Add files via upload
    to push files in git from java API

---
+++ TestConnection.java
@@ -0,0 +1,33 @@
+package com.git.api;
+
+import org.kohsuke.github.*;
+
+import java.io.IOException;
+
+public class TestConnection {
+    public static void main(String[] srgs) throws IOException {
+        //GitHub github = GitHub.connect();
+        //github.getRepository("https://github.com/mambora18/mambora18");
+        //GitHub github1 =  new GitHubBuilder().withPassword("mambora18","Michu2604#").build();
+
+        //System.out.println(github1.getUser("mambora18"));
+
+        //System.out.println(github1.getConnector().connect("https://github.com/mambora18/mambora18").)
+
+        //github1.getRepository("https://github.com/mambora18/mambora18").createPullRequest()
+
+
+
+//        github1.getRepository("mambora18/mambora18")
+//                .createPullRequest("first PR","head","base","body").isMerged();
+
+
+        GitHub github = new GitHubBuilder().withPassword("mambora18","Michu2604#").build();
+
+        GHRepository gitRepo = github.getRepository("mambora18/mambora18");
+        GHContentBuilder content = gitRepo.createContent().branch("master");
+        content.content("testing again").path("files/testfile.txt").branch("master").message("committing to master again").commit();
+
+
+    }
+}
