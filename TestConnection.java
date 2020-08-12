18d73aad0e9928bee12cd6c0d5d65e4ae2de99f4
mambora18 <69573856+mambora18@users.noreply.github.com>
2020-08-12 18:55:23 +0530

    Update TestConnection.java

--- TestConnection.java
+++ TestConnection.java
@@ -8,7 +8,7 @@ public class TestConnection {
     public static void main(String[] srgs) throws IOException {
         //GitHub github = GitHub.connect();
         //github.getRepository("https://github.com/mambora18/mambora18");
-        //GitHub github1 =  new GitHubBuilder().withPassword("mambora18","Michu2604#").build();
+        //GitHub github1 =  new GitHubBuilder().withPassword("mambora18","***").build();
 
         //System.out.println(github1.getUser("mambora18"));
 
@@ -22,7 +22,7 @@ public class TestConnection {
 //                .createPullRequest("first PR","head","base","body").isMerged();
 
 
-        GitHub github = new GitHubBuilder().withPassword("mambora18","Michu2604#").build();
+        GitHub github = new GitHubBuilder().withPassword("mambora18","***").build();
 
         GHRepository gitRepo = github.getRepository("mambora18/mambora18");
         GHContentBuilder content = gitRepo.createContent().branch("master");
