2feb4dcb1d46cb4943c096e5af69f1d75891aa8b
mambora18 <69573856+mambora18@users.noreply.github.com>
2020-08-12 20:28:00 +0530

    Update TestConnection.java
    updated to file save

--- TestConnection.java
+++ TestConnection.java
@@ -6,28 +6,16 @@ import java.io.IOException;
 
 public class TestConnection {
     public static void main(String[] srgs) throws IOException {
-        //GitHub github = GitHub.connect();
-        //github.getRepository("https://github.com/mambora18/mambora18");
-        //GitHub github1 =  new GitHubBuilder().withPassword("mambora18","***").build();
-
-        //System.out.println(github1.getUser("mambora18"));
-
-        //System.out.println(github1.getConnector().connect("https://github.com/mambora18/mambora18").)
-
-        //github1.getRepository("https://github.com/mambora18/mambora18").createPullRequest()
-
-
-
-//        github1.getRepository("mambora18/mambora18")
-//                .createPullRequest("first PR","head","base","body").isMerged();
-
-
         GitHub github = new GitHubBuilder().withPassword("mambora18","***").build();
 
         GHRepository gitRepo = github.getRepository("mambora18/mambora18");
         GHContentBuilder content = gitRepo.createContent().branch("master");
-        content.content("testing again").path("files/testfile.txt").branch("master").message("committing to master again").commit();
 
+        File file = new File ("C:\\MAMTA BORA\\testingBlob.txt");
+        FileInputStream stream = new FileInputStream(file);
+        byte[] bytes = stream.toString().getBytes();
+
+        content.content(bytes).path("blobFiles/testingBlob.txt").branch("master").message("committing to the blob files").commit();
 
     }
 }
