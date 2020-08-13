9b0d3dbb52800b9330db39403f3e95437c337b4e
mambora18 <69573856+mambora18@users.noreply.github.com>
2020-08-13 09:01:19 +0530

    Update TestConnection.java
    updated

--- TestConnection.java
+++ TestConnection.java
@@ -1,33 +1,14 @@
-package com.azure.function;
+package com.git.api;
 
-import java.util.*;
-import com.microsoft.azure.functions.annotation.*;
-import com.microsoft.azure.functions.*;
 import org.kohsuke.github.*;
 
 import java.io.*;
 
-/**
- * Azure Functions with HTTP Trigger.
- */
-public class HttpTriggerFunction {
-    /**
-     * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpTrigger-Java
-     * 2. curl {your host}/api/HttpTrigger-Java?name=HTTP%20Query
-     */
-    @FunctionName("HttpTriggerForGitPush")
-    public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) throws IOException {
-        context.getLogger().info("Java HTTP trigger processed a request.");
+public class TestConnection {
+    public static void main(String[] srgs) throws IOException {
 
-        // Parse query parameter
-        String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
-
-        //Logic to push code to git repo
-        GitHub github = new GitHubBuilder().withPassword("mambora18","***").build();
+        /*  code to push the file to git repository*/
+        GitHub github = new GitHubBuilder().withPassword("mambora18","****#").build();
 
         GHRepository gitRepo = github.getRepository("mambora18/mambora18");
         GHContentBuilder content = gitRepo.createContent().branch("master");
@@ -36,12 +17,8 @@ public class HttpTriggerFunction {
         FileInputStream stream = new FileInputStream(file);
         byte[] bytes = stream.toString().getBytes();
 
-        content.content(bytes).path("blobFiles/testingBlob1.txt").branch("master").message("committing to the blob files").commit();
+        content.content(bytes).path("blobFiles/testingBlob2.txt").branch("master").message("committing to the blob files").commit();
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
-        }
+
     }
 }
